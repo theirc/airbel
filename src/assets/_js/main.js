194988feb953a3b6abcc19fca2e3c8f709ecd74f
Jon Burr <yojonathanwburr@gmail.com>
2018-09-25 15:07:46 -0700

    filtering for projects

--- src/assets/_js/main.js
+++ src/assets/_js/main.js
@@ -11,4 +11,26 @@ $(function() {
   console.log('jQuery loaded');
 
   $('#focus-tabs').tabCollapse();
+
+  var mixer = mixitup('.projects', {
+    animation: {
+      effects: 'fade scale(0.5)'
+    },
+    callbacks: {
+      onMixClick: function(state, originalEvent) {
+        console.log('The control "' + this.innerText + '" was clicked');
+      },
+      onMixFail: function(state) {
+        console.log('No items could be found matching the requested filter');
+      }
+    }
+  });
+
+  $('.filter').on('click', function(){
+    var filter_val = $(this).data('filter');
+    var text = $(this).text();
+    $('#filter-text').text(text);
+  });
+
+  mixer.mixFail
 });
