b4a7eef7980810d5664e2a628fa5f9208449b550
Jon Burr <jonathanwburr@gmail.com>
2019-09-15 13:22:52 -0700

    team grid functionality

--- src/_scripts/App/components/team/TeamGrid.js
+++ src/_scripts/App/components/team/TeamGrid.js
@@ -1,11 +1,41 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import TeamMember from "./TeamMember"
 
+
+//function for randomizing array
+function shuffleArray(array) {
+  let i = array.length - 1;
+  for (; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+
+
 const TeamGrid = ({team}) => {
+
+  //pass array through shuffleArray function
+  const shuffledTeam = shuffleArray(team);
+
+  //define how many elements to show from array
+  // const itemsToShow = 4;
+  // const totalItems = team.length;
+
   return (
     <>
       <div className="row">
-        {team && team.map(team => (
+        {shuffledTeam && shuffledTeam.slice(0, 4).map(team => (
+          <>
+            <TeamMember team={team} />
+          </>
+        ))}
+      </div>
+      <div className="row">
+        {shuffledTeam && shuffledTeam.slice(4, 8).map(team => (
           <>
             <TeamMember team={team} />
           </>
