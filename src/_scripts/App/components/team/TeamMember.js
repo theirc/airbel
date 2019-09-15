93a2f9430d91c422450fbe2515ab0be5ce52cfc9
Jon Burr <jonathanwburr@gmail.com>
2019-09-14 17:35:04 -0700

    team member react component

--- src/_scripts/App/components/team/TeamMember.js
+++ src/_scripts/App/components/team/TeamMember.js
@@ -1,26 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
+
 
 const TeamMember = (team) => {
+  const [showMemberDetails, setShowMemberDetails] = useState(false);
   const BackgroundImage = {
     backgroundImage: `url(` + team.team.image + `)`,
   }
   return (
     <>
       <div className="col-lg-3">
-        <div className="bg-image aspect-1x1" style={BackgroundImage}></div>
+        <div className="bg-image aspect-1x1" style={BackgroundImage} onClick={() => setShowMemberDetails(!showMemberDetails)}></div>
+        
+      </div>
+      {showMemberDetails && <div className="team-member-details">
         <h3>{team.team.name}</h3>
         <p><em>{team.team.role_title}</em></p>
-        <hr/>
-        <p><strong>Focus:</strong><br/>
+        <hr />
+        <p><strong>Focus:</strong><br />
           {team.team.focuses}
         </p>
-        <p><strong>Practice:</strong><br/>
+        <p><strong>Practice:</strong><br />
           {team.team.practice}
         </p>
-        <hr/>
+        <hr />
         <p>{team.team.bio}</p>
         {team.team.leadership}
-      </div>
+      </div>}
     </>
   )
 }
