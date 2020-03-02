850e271ab7eb9b3ffce6c157b85c59827715d2b3
tombeck <t@ocupop.com>
2020-03-02 15:25:25 -0800

    fixes for security vulnerabilities

--- src/_scripts/App/components/HelloWorld.js
+++ src/_scripts/App/components/HelloWorld.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class HelloWorld extends Component {
   render() {
     const { title } = this.props;
     return (
       <div>
-        <h1>Hello Mom!</h1>
-        <p><strong>{title}</strong></p>
+        <h1>Hello World!</h1>
+        <p>
+          <strong>{title}</strong>
+        </p>
       </div>
-    )
+    );
   }
 }
